0f570322b4ce33b4b9d485ed12bb4af27d2cb23c
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2015-12-29 16:43:02 +0300

    fix clinit for hdfs-ready

--- roles/hadoop/templates/bin/hdfs-ready.sh
+++ roles/hadoop/templates/bin/hdfs-ready.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-
-log_filename="{{ log_folder }}/hdfs/hdfs-ready.log"
 ready_filename="/hdfs_ready"
-
-while true; do
-  echo "`date -u`: check hdfs" >>$log_filename
-  sudo -Hu hdfs hdfs dfs -touchz $ready_filename >>$log_filename 2>&1 && break
-  echo >>$log_filename
-  sleep 10
-done
-sudo -Hu hdfs hdfs dfs -rm $ready_filename >>$log_filename 2>&1
-echo >>$log_filename
-
-exit 0
+sudo -Hu hdfs timeout 10 hdfs dfs -touchz $ready_filename &>/dev/null && exit 0
+exit 1
 
